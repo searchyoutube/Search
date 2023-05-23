8ca7e8ed72672eb3a0f281ea96fd04716a756d9c
TiEM <a52675268@gmail.com>
2023-05-23 21:43:25 +0900

    predict code update
    장고에서 사용을 위한 predict_video.py 코드 제작

--- YoutubeDATA/predict_video.py
+++ YoutubeDATA/predict_video.py
@@ -1,12 +1,13 @@
 from keras.models import load_model
 import numpy as np
+from tensorflow_addons.optimizers import RectifiedAdam
 import pandas as pd
 from sklearn.preprocessing import RobustScaler
 
 # make dummy column own my way
 def make_dummy_category(data):
     for i in range(27):
-        data[f"category_{i+1}"] = [1 if category == (i+1) else 0 for category in data['category_id']]
+        data.loc[:, f"category_{i+1}"] = [1 if category == (i+1) else 0 for category in data['category_id']]
     return data
 
 def categorize_duration(duration):
@@ -35,59 +36,58 @@ def prepare_data(data, n_steps):
     return np.array(X), np.array(y)
 
 #데이터 전처리 과정
-def video_preprocess(datas):
+def video_preprocess(data):
     """
     데이터 전처리
     input: pandas dataframe
     output: pandas dataframe
     """
-    data = make_dummy_category(data)
-    data.drop(['category_id'],axis=1,inplace=True)
-    data['duration'] = data['duration'].apply(categorize_duration)
 
-    datas['views'] = datas['views'].astype(int)
-
-    datas['views_diff'] = datas['views'].diff()
+    data['views_diff'] = data['views'].diff()
 
     viewScaler = RobustScaler()
-    datas['views_diff_scaled'] = viewScaler.fit_transform(datas['views_diff'].to_numpy().reshape(-1,1))
+    data['views_diff_scaled'] = viewScaler.fit_transform(data['views_diff'].to_numpy().reshape(-1,1))
 
-    datas['likes_diff'] = datas['likes'].diff()
+    data['likes_diff'] = data['likes'].diff()
 
-    datas.drop(datas.index[0], inplace=True)
+    data.drop(data.index[0], inplace=True)
 
     likeScaler = RobustScaler()
     # Scale the likes column, ignoring missing values
-    datas['likes_diff_scaled'] = viewScaler.fit_transform(datas['likes_diff'].to_numpy().reshape(-1,1))
+    data['likes_diff_scaled'] = likeScaler.fit_transform(data['likes_diff'].to_numpy().reshape(-1,1))
 
-    datas.drop(['Unnamed: 0'],axis=1,inplace=True)
+    columns = ['category_id', 'duration','views_diff_scaled', 'likes_diff_scaled','hour']
+    data = data[columns]
+    data = make_dummy_category(data)
+    data.drop(['category_id'],axis=1,inplace=True)
+    data['duration'] = data['duration'].apply(categorize_duration)
+    return data, viewScaler, likeScaler
 
-    return datas, viewScaler, likeScaler
-
-def predict(data):
+def predict_hour(data):
     initX, inity = np.zeros((1,3,6)), np.zeros((1,2))
     X = np.zeros((1,3,6))
     Y = np.zeros((1,2))
-
+    
     data, viewScaler, likesScaler = video_preprocess(data)
     #simple pre-processing to make one hot vector for category.
+    data = data[-3:]
+    
 
     if data.isnull().any().any():
         # print(f"there is null in {filename}")
         return "데이터 오류"
 
-    
+    data = np.array(data)
+    custom_objects = {'RectifiedAdam': RectifiedAdam}
+    model = load_model('hour_model.h5', custom_objects=custom_objects)
 
-    # prepare the data using the prepare_data() function
-    tmpX, tmpy = prepare_data(data, n_steps=3)
+    pre_view,pre_like = model.predict(data.reshape(1,3,31))[0]
+    view = viewScaler.inverse_transform(pre_view.reshape(-1, 1))
+    like = likesScaler.inverse_transform(pre_like.reshape(-1, 1))
+    return float(view), float(like)
 
-    if not np.array_equal(X,initX):
-        if (tmpX.shape[1:3] == X.shape[1:3]):
-            X = np.concatenate((X,tmpX),axis=0)
-            Y = np.concatenate((Y,tmpy),axis=0)
-    else:
-        X = tmpX
-        Y = tmpy
-    
-    model = load_model('hour_model.h5')
-    model.predict(X[0].reshape(1,3,32))
+if __name__ == "__main__":
+    data = pd.read_csv(r'testdata.csv')
+    preview,prelike = predict_hour(data)
+    print(preview)
+    print(prelike)
